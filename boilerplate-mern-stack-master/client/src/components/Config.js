60680078d1c41254330bea1a678a1c9eb91b58df
Tomady <87797173+Tomady@users.noreply.github.com>
2022-01-24 22:28:37 +0900

    Delete boilerplate-mern-stack-master directory

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++
@@ -1,5 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'a7ba850935c47a15642eadaa24e7a888';
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
--- boilerplate-mern-stack-master/server/config/dbConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    user: process.env.NODEORACLEDB_USER || "prj",
-    password: process.env.NODEORACLEDB_PASSWORD || "1234",
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "local/xe"
-}
